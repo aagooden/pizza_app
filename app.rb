abe252672470262505b2e9a512bf4704b5b9611a
RandyCoffman <randy.coffman@wvjc.edu>
2018-04-10 10:41:56 -0400

    first commit

---
+++ app.rb
@@ -0,0 +1,149 @@
+require 'sinatra'
+require_relative 'refactored_pizza.rb'
+
+enable :sessions
+
+get '/' do
+	session[:total_price] = 0
+	erb :welcome_page0
+end
+
+post '/on-to-size' do
+	redirect '/size'
+end
+
+get '/size' do
+	session[:pizza] = []
+	erb :pizza_size_page1
+end
+
+post '/on-to-crust' do
+	session[:pizza].push(params.values)
+	redirect '/crust'
+end
+
+get '/crust' do
+	p session[:pizza]
+	erb :pizza_crust_page2
+end
+
+post '/on-to-meats' do
+	session[:pizza].push(params.values)
+	redirect '/meats'
+end
+
+get '/meats' do
+	erb :meats_page3
+end
+
+post '/on-to-veggies' do
+	session[:pizza].push(params.values)
+	redirect '/veggies'
+end
+
+get '/veggies' do
+	erb :veggies_page4
+end
+
+post '/on-to-special-toppings' do
+	session[:pizza].push(params.values)
+	redirect '/special-toppings'
+end
+
+get '/special-toppings' do
+	erb :special_toppings_page5
+end
+
+post '/on-to-sauces' do
+	session[:pizza].push(params.values)
+	redirect '/sauces'
+end
+
+get '/sauces' do
+	erb :sauces_page6
+end
+
+post '/on-to-extra-toppings' do
+	session[:pizza].push(params.values)
+	redirect '/extra-toppings'
+end
+
+get '/extra-toppings' do
+	session[:pizza].push(params.values)
+	erb :extra_toppings_page7
+end
+
+post '/on-to-salad' do
+	session[:pizza].push(params.values)
+	redirect '/salad'
+end
+
+get '/salad' do
+	erb :salad_page8
+end
+
+post '/on-to-wings' do
+	session[:pizza].push(params.values)
+	redirect '/wings'
+end
+
+get '/wings' do
+	erb :wings_page9
+end
+
+post '/on-to-drinks' do
+	session[:pizza].push(params.values)
+	redirect '/drinks'
+end
+
+get '/drinks' do
+	erb :drinks_page10
+end
+
+post '/on-to-pasta' do
+	session[:pizza].push(params.values)
+	redirect '/pasta'
+end
+
+get '/pasta' do
+	erb :pasta_page11
+end
+
+post '/on-to-almost-final' do
+	session[:pizza].push(params.values)
+	redirect '/almost-final'
+end
+
+get '/almost-final' do
+	pizza = session[:pizza].flatten
+	pizza.each do |prices|
+		session[:total_price] += prices.to_i
+	end
+	p session[:total_price]
+	erb :almost_final_page12
+end
+
+post '/on-to-final' do
+	redirect '/final'
+end
+
+get '/final' do
+	erb :final_page13, locals:{total_price: session[:total_price]}
+end
+
+get '/more-pizza' do
+	session[:pizza].clear
+	redirect "/size"
+end
+
+get '/news' do
+	erb :news
+end
+
+get '/about' do
+	erb :about
+end
+
+get '/contact' do
+	erb :contact
+end
---
+++ refactored_pizza.rb
@@ -0,0 +1,43 @@
+def size
+	{"small" => 2, "medium" => 3, "large" => 4, "XL" => 5}
+end
+
+def crust
+	{"Pan crust" => 0, "Stuffed crust" => 2, "Bacon stuffed crust" => 3}
+end
+
+def meats
+	{"Chicken" => 2, "Pepperoni" => 1, "Sausage" => 1, "Bacon" => 2}
+end
+
+def veggies
+	{"Tomato" => 1, "Pineapple" => 2, "Spinach" => 1}
+end
+
+def special_toppings
+	{"Bbq sauce" => 2, "Buffalo sauce" => 2}
+end
+
+def sauces
+	{"Alfredo" => 0, "Bbq" => 0, "Tomato" => 0, "Marinara" => 0}
+end
+
+def extra_toppings
+	{"Double pepperoni" => 1, "Extra cheese" => 1, "Double chicken" => 3,}
+end
+
+def salad
+	{"Caesar" => 10, "Veggie" => 10}
+end
+
+def wings
+	{"Kentucky bourbon" => 2, "Garlic" => 2, "Buffalo" => 3}
+end
+
+def drinks
+	{"Water" => 1, "Coca-Cola" => 2, "Powerade" => 2, "Tea" => 2, "Sprite" => 2}
+end
+
+def pasta
+	{"Chicken alfredo" => 8, "Lasagne" => 8}
+end
