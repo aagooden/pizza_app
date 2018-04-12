1ed910a5b1f35969b47805198d06db7c8533feff
RandyCoffman <randy.coffman@wvjc.edu>
2018-04-12 13:31:19 -0400

    I fixed some naming and added in an option to have none

--- refactored_pizza.rb
+++ refactored_pizza.rb
@@ -1,5 +1,5 @@
 def size
-	{"small" => 2, "medium" => 3, "large" => 4, "XL" => 5}
+	{"small crust" => 2, "medium crust" => 3, "large crust" => 4, "XL crust" => 5}
 end
 
 def crust
@@ -7,37 +7,37 @@ def crust
 end
 
 def meats
-	{"Chicken" => 2, "Pepperoni" => 1, "Sausage" => 1, "Bacon" => 2}
+	{"none" => 0, "Chicken" => 2, "Pepperoni" => 1, "Sausage" => 1, "Bacon" => 2}
 end
 
 def veggies
-	{"Tomato" => 1, "Pineapple" => 2, "Spinach" => 1}
+	{"none" => 0, "Tomato" => 1, "Pineapple" => 2, "Spinach" => 1}
 end
 
 def special_toppings
-	{"Bbq sauce" => 2, "Buffalo sauce" => 2}
+	{"none" => 0, "Bbq sauce" => 2, "Buffalo sauce" => 2}
 end
 
 def sauces
-	{"Alfredo" => 0, "Bbq" => 0, "Tomato" => 0, "Marinara" => 0}
+	{"none" => 0, "Alfredo sauce" => 0, "Bbq sauce" => 0, "Tomato sauce" => 0, "Marinara sauce" => 0}
 end
 
 def extra_toppings
-	{"Double pepperoni" => 1, "Extra cheese" => 1, "Double chicken" => 3,}
+	{"none" => 0, "Double pepperoni" => 1, "Extra cheese" => 1, "Double chicken" => 3,}
 end
 
 def salad
-	{"Caesar" => 10, "Veggie" => 10}
+	{"none" => 0, "Caesar" => 10, "Veggie" => 10}
 end
 
 def wings
-	{"Kentucky bourbon" => 2, "Garlic" => 2, "Buffalo" => 3}
+	{"none" => 0, "Kentucky bourbon" => 2, "Garlic" => 2, "Buffalo" => 3}
 end
 
 def drinks
-	{"Water" => 1, "Coca-Cola" => 2, "Powerade" => 2, "Tea" => 2, "Sprite" => 2}
+	{"none" => 0, "Water" => 1, "Coca-Cola" => 2, "Powerade" => 2, "Tea" => 2, "Sprite" => 2}
 end
 
 def pasta
-	{"Chicken alfredo" => 8, "Lasagne" => 8}
+	{"none" => 0, "Chicken alfredo" => 8, "Lasagne" => 8}
 end
